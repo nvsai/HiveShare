4ea7f9cde173dd6ae204afea7286858d38ffb205
nvsai <nagavenkatasai246@gmail.com>
2023-10-30 01:05:55 +0530

    Added function recieve_d to send host device name to other device.

--- device.py
+++ device.py
@@ -7,3 +7,31 @@ def get_ip():
 def get_dname():
     return socket.gethostname()
 
+def receive_d():
+    my_ip=get_ip()
+    PORT_NUMBER = 5000
+    SIZE = 1024
+    message=""
+    hostName = socket.gethostbyname( '0.0.0.0' )
+    mySocket=socket.socket(AF_INET, SOCK_DGRAM)
+    mySocket.bind( (hostName, PORT_NUMBER) )
+    device_name=socket.gethostname()
+
+    print("Test server listening on port {0}\n".format(PORT_NUMBER))
+    out="Test server listening on port {0}\n".format(PORT_NUMBER)
+   
+
+    while True:
+        (data,addr) = mySocket.recvfrom(SIZE)
+        break
+
+    print(data.decode('utf-8'))
+    data=data.decode('utf-8')
+    mySocket.close()
+
+    otherSocket=socket.socket(AF_INET, SOCK_DGRAM)
+    time.sleep(3)
+    otherSocket.connect((data,PORT_NUMBER))
+    otherSocket.send(device_name.encode('utf-8'))
+    otherSocket.close()
+    return "connecting"
